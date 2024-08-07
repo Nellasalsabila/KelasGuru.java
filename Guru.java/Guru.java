f0941bb48dd19a4ad670e91cd93d9632241ed572
Nellasalsabila <nella_salsabila_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:55:20 +0700

    Add files via upload

---
+++ Guru.java/Guru.java
@@ -0,0 +1,15 @@
+//Objek Class
+public class Guru {
+    int id;
+    String nama;
+    String mapel;
+    String alamat;
+
+    //Constructor nama dng Class//
+    public Guru(){
+        id = 0;
+        nama = "";
+        mapel ="";
+        alamat = "";
+    }
+}
---
+++ Guru.java/TesGuru.java
@@ -0,0 +1,73 @@
+public class TesGuru {
+
+    int id;
+    String nama;
+    Object mapel;
+    String alamat;
+
+    public static void main(String[] args) {
+    //membuat obyek
+    TesGuru pasha = new TesGuru();
+    pasha.id =2;
+    pasha.nama ="Pasha";
+    pasha.mapel ="Produktif";
+    pasha.alamat ="Malang"; 
+    System.out.println("Ini data pasha");
+    System.out.println(pasha.id);
+    System.out.println(pasha.nama);
+    System.out.println(pasha.mapel);
+    System.out.println(pasha.alamat);
+
+
+
+    TesGuru feniliya = new TesGuru();
+    feniliya.id =10;
+    feniliya.nama ="feniliya";
+    feniliya.mapel ="Matematika";
+    feniliya.alamat ="Malang"; 
+    System.out.println("Ini data feniliya");
+    System.out.println(feniliya.id);
+    System.out.println(feniliya.nama);
+    System.out.println(feniliya.mapel);
+    System.out.println(feniliya.alamat);
+
+
+
+    TesGuru aulia = new TesGuru();
+    aulia.id =20;
+    aulia.nama ="Aulia";
+    aulia.mapel ="Produktif";
+    aulia.alamat ="Malang"; 
+    System.out.println("Ini data aulia");
+    System.out.println(aulia.id);
+    System.out.println(aulia.nama);
+    System.out.println(aulia.mapel);
+    System.out.println(aulia.alamat);
+
+
+
+    TesGuru andre = new TesGuru();
+    andre.id =30;
+    andre.nama ="Andre";
+    andre.mapel ="bahasa inggris";
+    andre.alamat ="Malang"; 
+    System.out.println("Ini data andre");
+    System.out.println(andre.id);
+    System.out.println(andre.nama);
+    System.out.println(andre.mapel);
+    System.out.println(andre.alamat);
+
+
+
+    TesGuru diaur = new TesGuru();
+    diaur.id =40;
+    diaur.nama ="Diaur";
+    diaur.mapel ="ipas";
+    aulia.alamat ="Malang"; 
+    System.out.println("Ini data Diaur");
+    System.out.println(diaur.id);
+    System.out.println(diaur.nama);
+    System.out.println(diaur.mapel);
+    System.out.println(diaur.alamat);
+    }
+}
